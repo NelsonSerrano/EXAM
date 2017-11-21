37cbbd6fa91ab15bd09ecd10b75d4194802ff79a
NelsonSerrano <nserranoherrera@gmail.com>
2017-11-21 10:28:21 -0300

    Add files via upload

---
+++ acerca.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es-cl">
+<head>
+	<meta charset="UTF-8">
+	<title>ExamP</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="./js/jquery-3.2.1.min.js"></script>
+    <script src="./js/main.js"></script>
+    <!-- <script src="./js/jquery.rut.chileno.min.js"></script> -->
+	<link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <style>
+        .acerca{
+            margin-top: 2em;
+        }
+    </style>
+    </head>
+<body>
+      <!-- inicio de navegacion  -->
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+         <a class="navbar-brand" href="index.php"><img src="images/logo.jpg" class="logo" alt=""></a>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+          <a class="nav-link" href="index.php">Inicio<span class="sr-only">(current)</span></a>
+          </li>
+      
+          <li class="nav-item">
+          <a class="nav-link" href="acerca.php">Acerca de Lexam</a>
+          </li>
+        </ul>
+      <span class="navbar-text">
+         
+      </span>
+    </div>
+</nav>
+<!-- termino de navegacion -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10 acerca">
+            <h1>LEXAM</h1>
+            <p><strong>Lexam es un sistema de evaluación en línea,</strong> a través de una red local en la cual interactúan los alumnos con el profesor, respondiendo pruebas o exámenes de rápida corrección, con respuestas cerradas.
+            Los usuarios rápidamente podrán verificar los resultados y para el profesor será posible diagnosticar y evaluar el desempeño individual o grupal del curso.
+            Asimismo, Lexam provee la posibilidad de evaluar habilidades asociadas a las preguntas diseñadas por el profesor, para conocer los niveles de logro según criterios, ejes, categorías o habilidades que se consideren pertinentes y que configuren la prueba que se ha diseñado.
+            <strong>El diseño y desarrollo de esta plataforma constituye un derecho reservado. Prohibida su reproducción o distribución sin licenciamiento o contrato de uso.</strong>
+            </p>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+</div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
